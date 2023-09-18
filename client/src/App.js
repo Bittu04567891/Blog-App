37b3d59995275944be012dba65513ff94d9189b9
Bittu <kumarbittu775@gmail.com>
2023-09-19 01:05:55 +0530

    write page and single post page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import Home from "./pages/home/Home.jsx";
+// import Home from "./pages/home/Home.jsx";
 import Topbar from "./components/topbar/topbar.jsx";
-import { Single } from "./pages/single/Single.jsx";
+import Write from "./pages/write/Write.jsx";
+// import { Single } from "./pages/single/Single.jsx";
 
 function App() {
   return (
     <>
       <Topbar />
-      <Single />
+      {/* <Single /> */}
+      <Write />
     </>
   );
 }
--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -16,6 +16,36 @@ const SinglePost = () => {
             <i className="singlePostIcon fa-solid fa-trash"></i>
           </div>
         </h1>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Author: <b>Bittu</b>
+          </span>
+          <span className="singlePostDate">1 hour ago</span>
+        </div>
+        <p className="singlePostDesc">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+          exercitationem ducimus voluptate ex, perspiciatis voluptatibus earum
+          voluptates reiciendis neque, cupiditate, tenetur architecto nisi
+          obcaecati itaque ratione eos facere magni aliquam! Lorem, ipsum dolor
+          sit amet consectetur adipisicing elit. Vel, voluptatum dolor
+          voluptate, impedit ab cum iste quo quisquam minus nihil similique quis
+          nulla aliquam totam deleniti commodi fugit. Omnis, tenetur neque
+          blanditiis deserunt eligendi, facilis aut exercitationem voluptate
+          expedita enim, possimus facere minus repellendus dolore aperiam
+          commodi reiciendis inventore. Eveniet quisquam ea laudantium eligendi
+          nulla ipsa dignissimos aut in minus, ut vero laborum, doloribus,
+          pariatur dolorem? Tempora reprehenderit expedita iure reiciendis
+          necessitatibus cumque magnam quae possimus modi optio magni, ut velit
+          non rerum dignissimos exercitationem itaque, inventore repellat!
+          Delectus atque eveniet facere omnis quaerat enim ab nihil tempore?
+          Quam unde deleniti eaque quibusdam illum officiis corrupti, laborum
+          placeat, nostrum sed modi magnam totam possimus id iusto fugit
+          perspiciatis labore exercitationem voluptatibus aspernatur similique.
+          Delectus, ducimus. Distinctio fuga nostrum facere aperiam doloribus,
+          est adipisci nisi veritatis soluta modi mollitia ullam dignissimos
+          voluptatibus sint saepe sequi exercitationem libero magnam. Totam,
+          sunt obcaecati.
+        </p>
       </div>
     </div>
   );
