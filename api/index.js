7f04932e3f955cd6a8ea97681dccf2be179b6bf9
Bittu <kumarbittu775@gmail.com>
2023-09-29 00:53:29 +0530

    delete update

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -11,6 +14,9 @@ mongoose
     console.log("DB connection successful");
   })
   .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen("5000", () => {
   console.log("Backend Server running!");
