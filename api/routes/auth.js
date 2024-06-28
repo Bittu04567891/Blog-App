19b81a8bfc1d29bd0975a34a5838d577545879cf
Bittu <kumarbittu775@gmail.com>
2024-06-28 06:34:00 +0530

    removed bcrypt in login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -27,7 +27,8 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({ username: req.body.username });
     !user && res.status(400).json("Wrong credentials");
 
-    const validated = await bcrypt.compare(req.body.password, user.password);
+    // const validated = await bcrypt.compare(req.body.password, user.password);
+    const validated = req.body.password === user.password;
     !validated && res.status(400).json("Wrong credentials");
 
     const { password, ...others } = user._doc;
